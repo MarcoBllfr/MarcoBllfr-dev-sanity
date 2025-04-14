3d37a7e7d9fd5c1954c021f1886bf81d6f7c319d
marcobllfr <122992858+Eramood@users.noreply.github.com>
2025-04-14 18:48:51 +0200

    rule update

--- schemaTypes/project.ts
+++ schemaTypes/project.ts
@@ -15,7 +15,6 @@ export const project = defineType({
             name: 'company',
             title: 'Company',
             type:'string',
-            validation: (rule) => rule.required() 
         }),
         defineField({
             name: 'slug',
